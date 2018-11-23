ed511c5db09fcae632a5bd2ba87e3e484d638fee
boyuan <boyuan459@hotmail.com>
2018-11-23 15:53:48 +1100

    email service init

--- routes/emails.js
+++ routes/emails.js
@@ -4,7 +4,14 @@ var asyncMiddleware = require('../middleware/async').asyncMiddleware;
 
 /* GET users listing. */
 router.post('/', asyncMiddleware(async (req, res, next) => {
-  
+  const to = req.body.to;
+  const cc = req.body.cc;
+  const bcc = req.body.bcc;
+  const subject = req.body.subject;
+  const text = req.body.text;
+  res.send({
+      to,cc,bcc,subject,text
+  })
 }));
 
 module.exports = router;
